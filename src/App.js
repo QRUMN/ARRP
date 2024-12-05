7936566ba1b03829c1d16509a8eec4a208600576
QRUMN <quorumnova@gmail.com>
2024-12-05 13:57:30 -0500

    Restructure project for Netlify deployment

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Home from './components/home/Home';
+import InspectorDashboard from './components/inspector/InspectorDashboard';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/inspector/dashboard" element={<InspectorDashboard />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+export default App;
